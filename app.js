4d159886fa345edf3af53d6ae624f324c9d9a93e
lodi palle <lodipalle@gmail.com>
2020-10-05 18:11:26 +1100

    added functionalities for app.js

--- app.js
+++ app.js
@@ -76,10 +76,26 @@ const menu = [
 const menuItems = document.querySelector(".section-center");
 
 window.addEventListener("DOMContentLoaded", function () {
-  let displayMenu = menu.map(function (item) {
-    console.log(item);
-  
-return item;
-  });
-  console.log(displayMenu);
+
 });
+
+function diplayMenuItems(menuItems) {
+    let displayMenu = menuItems.map(function (item) {
+    // console.log(item);
+  
+    return `<article class="menu-item">
+    <img src="${item.img}" class="photo" alt=${item.title}>
+    <div class="item-info">
+      <header>
+        <h4>${item.title}</h4>
+        <h4 class="price">$${item.title}</h4>
+      </header>
+      <p class="item-text">
+        ${item.desc}
+      </p>
+    </div>
+  </article>`;
+  });
+  displayMenu = displayMenu.join("");
+  menuItems.innerHTML = displayMenu;
+}
